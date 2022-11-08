ac8ed1420a8399055f6398988545737c372fc675
Sonanielectronics <100422141+Sonanielectronics@users.noreply.github.com>
2022-11-08 17:46:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 
 // var React = require("react");
 import React from "react";
+import "./index.css"
 
 // var ReactDOM = require('react-dom');
 import ReactDOM  from "react-dom";
 
-var name = "Ghoghari" ;
+var num = "num";
+var ber = "ber";
+var fname = "Ghoghari" ;
+var lname = "nikunj" ;
+const currentdate = new Date().toLocaleDateString();
+const currenttime = new Date().toLocaleTimeString();
+
+var styleinline = {
+    color:"red",
+    marginLeft:"10px"
+};
+
+// const checkdate = new Date(2020,5,5,20).getHours();
+const checkdate = new Date(2020,5,5,20).getHours();
+let val = " " ;
+var valsty = { } ;
+
+if(checkdate >= 1 && checkdate < 12){
+    val = "Good morning";
+}else if(checkdate >= 12 && checkdate < 19 ){
+    val = "Good afternoon";
+}else{
+    val = "Good night";
+}
+// for inline css we are add all property in object and specially key as well as pair value and here we are not use camlecase but we are use kabab 
 
 // ReactDOM.render(<div><h1>Hi</h1><h2>Nik</h2></div>,document.getElementById("root"));  // after react 16 we are use this 
 // ReactDOM.render([<h1>Hi</h1>,<h2>Niku</h2>],document.getElementById("root")); // first we are use this
 // ReactDOM.render(<React.Fragment><h1>Hi</h1><h2>Niku</h2></React.Fragment>,document.getElementById("root"));
 // ReactDOM.render(<><h1>Hi</h1><h2>Niku</h2></>,document.getElementById("root")); // this is also react fragment
-ReactDOM.render(<><h1>Hi</h1><h2>{name} you are the number { 0 + 1 } person from { Math.random() } </h2></>,document.getElementById("root")); // if we are use javascript under html then it is expressions // we can't use condition here
+ReactDOM.render(<><h1 className="hed">Hi</h1><h2 style={styleinline}>{ fname + " " + lname } you are the { `${num} ${ber}` } { 0 + 1 } person from { Math.random() } & today date is { currentdate } & current time is ................................................................................................... { currenttime }  </h2><h3 contentEditable="true">this is JSX Attributes</h3><img src="https://picsum.photos/200/300" alt="This"/><a href="https://picsum.photos/" target="_https://picsum.photos/"><img src="https://picsum.photos/200/300" alt="This"/></a><h4>{val}</h4></>,document.getElementById("root")); // if we are use javascript under html then it is expressions // we can't use condition here // we are use backtick into variable this is template literals 
+// image tag is self closing tool // in a tag we are use href under target _blank or any after _ to open into new window
